64990ee5207da0543c80b152e32a9f91707cc17c
Romi Qelibari <bajram.qelibari@googlemail.com>
2021-04-16 14:07:51 +0200

    add test_returns2IfBitsequenceLengthIs1

--- bitsequence/bitsequence.py
+++ bitsequence/bitsequence.py
@@ -1,2 +1,4 @@
 def bitsequence(sequence_lenght: int):
+    if sequence_lenght > 0:
+        return 2
     return 0
--- bitsequence/test_bitsequence.py
+++ bitsequence/test_bitsequence.py
@@ -6,3 +6,7 @@ class LengthRelatedTestCase(TestCase):
     def test_returns0IfBitsequenceLengthIs0(self):
         possibilities = bitsequence(0)
         self.assertEqual(0, possibilities)
+
+    def test_returns2IfBitsequenceLengthIs1(self):
+        possibilities = bitsequence(1)
+        self.assertEqual(2, possibilities)
