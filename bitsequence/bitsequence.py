0cea91295e60ca1ee1ffbdc5c755cfaf122fe21c
Romi Qelibari <bajram.qelibari@googlemail.com>
2021-04-18 01:23:51 +0200

    Add finished possibilities function

--- bitsequence/bitsequence.py
+++ bitsequence/bitsequence.py
@@ -1,4 +1,10 @@
+from functools import cache
+
+@cache
 def get_bitsequence_possibilities(sequence_length: int):
-    if sequence_length > 0:
-        return sequence_length + 1
-    return 0
+    if sequence_length <= 0: return 0
+    if sequence_length == 1: return 2
+    if sequence_length == 2: return 3
+    previous_result = get_bitsequence_possibilities(sequence_length - 1)
+    previous_zeros = get_bitsequence_possibilities(sequence_length - 2)
+    return previous_result + previous_zeros
--- bitsequence/test_bitsequence.py
+++ bitsequence/test_bitsequence.py
@@ -14,3 +14,19 @@ class LengthRelatedTestCase(TestCase):
     def test_returns3fIfBitsequenceLengthIs2(self):
         possibilities = get_bitsequence_possibilities(2)
         self.assertEqual(3, possibilities)
+
+    def test_returns5IfBitsequenceLengthIs3(self):
+        possibilities = get_bitsequence_possibilities(3)
+        self.assertEqual(5, possibilities)
+
+    def test_returns8IfBitsequenceLengthIs4(self):
+        possibilities = get_bitsequence_possibilities(4)
+        self.assertEqual(8, possibilities)
+
+    def test_returns13IfBitsequenceLengthIs5(self):
+        possibilities = get_bitsequence_possibilities(5)
+        self.assertEqual(13, possibilities)
+
+    def test_returns13IfBitsequenceLengthIs6(self):
+        possibilities = get_bitsequence_possibilities(6)
+        self.assertEqual(21, possibilities)
