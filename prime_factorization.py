bbea0b72e9c22686b4ebf0cefb77310c5b7fd494
Romi Qelibari <bajram.qelibari@googlemail.com>
2021-04-20 15:32:36 +0200

    Add prime_factorization.py

--- prime_factorization.py
+++ prime_factorization.py
@@ -1,4 +1,28 @@
-def factorize(num):
-    if num > 1:
-        return '2'
-    return None
+from math import sqrt, floor
+
+
+def factorize(num: int) -> list[int]:
+    result, num = return_list_of_twos_and_new_num(num)
+    other_factors, rest = return_list_of_other_primefactors_and_rest(num)
+    result.extend(other_factors)
+    result.append(int(rest))
+    if result[-1] == 1:
+        return result[0:-1]
+    return result
+
+
+def return_list_of_twos_and_new_num(num):
+    result = []
+    while num % 2 == 0:
+        result.append(2)
+        num = num / 2
+    return result, num
+
+
+def return_list_of_other_primefactors_and_rest(num: int):
+    result = []
+    for divisor in range(3, int(sqrt(num)) + 1):
+        while num % divisor == 0:
+            result.append(divisor)
+            num = num / divisor
+    return result, num
--- test_prime_factorization.py
+++ test_prime_factorization.py
@@ -4,4 +4,22 @@ from prime_factorization import *
 
 class PrimeFactorizationTestCase(TestCase):
     def test_returnsPrimeIfNumIsPrime(self):
-        self.assertEqual('2', factorize(2))
+        self.assertEqual([2], factorize(2))
+        self.assertEqual([3], factorize(3))
+        self.assertEqual([5], factorize(5))
+
+    def test_returnListOfTwosAndNewNum(self):
+        self.assertEqual(([2, 2], 1), return_list_of_twos_and_new_num(4))
+        self.assertEqual(([2, 2], 141), return_list_of_twos_and_new_num(564))
+
+    def test_sixReturnsItsPrimefactorization(self):
+        self.assertEqual([2, 3], factorize(6))
+
+    def test_nineReturnsPrimefactorization(self):
+        self.assertEqual([3, 3], factorize(9))
+
+    def test_returnListOfOtherPrimefactorsAndRest(self):
+        self.assertEqual(([], 3), return_list_of_other_primefactors_and_rest(3))
+        self.assertEqual(([], 5), return_list_of_other_primefactors_and_rest(5))
+        self.assertEqual(([3], 47), return_list_of_other_primefactors_and_rest(141))
+        self.assertEqual(([7], 257), return_list_of_other_primefactors_and_rest(1799))
